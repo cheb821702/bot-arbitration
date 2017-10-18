79e8a0152de19b71c8ebe46895d408a85c9b0492
cheb821702 <cheb821702@mail.ru>
2017-10-19 01:41:34 +0300

    Update parser.

--- src/main/java/com/crypto/cryptocompare/JavaApiGenerator.java
+++ src/main/java/com/crypto/cryptocompare/JavaApiGenerator.java
@@ -8,55 +8,97 @@ import com.google.gson.stream.JsonToken;
 
 public class JavaApiGenerator {
 
-	private ApiJsonProvider apiProvider = new ApiJsonProvider();
+    private ApiJsonProvider apiProvider = new ApiJsonProvider();
 
-	public static void main(String[] args) {
-		new JavaApiGenerator().testIt();
-	}
+    public static void main(String[] args) {
+        new JavaApiGenerator().testIt();
+    }
 
-	public void testIt() {
-		generateApiFromJSONSpecification(apiProvider.getSpecificationReader());
-	}
+    public void testIt() {
+        generateApiFromJSONSpecification(apiProvider.getSpecificationReader());
+    }
 
-	public void generateApiFromJSONSpecification(InputStreamReader textStream) {
-		if (textStream != null) {
-			JsonReader jsonReader = new JsonReader(textStream);
-			try {
-				parceRoot(jsonReader);
-				jsonReader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    public void generateApiFromJSONSpecification(InputStreamReader textStream) {
+        if (textStream != null) {
+            JsonReader jsonReader = new JsonReader(textStream);
+            try {
+                parseRoot(jsonReader);
+                jsonReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	private void parceRoot(JsonReader reader) throws IOException {
-		while (reader.hasNext()) { // обходим все токены
-			JsonToken jsonToken = reader.peek(); // получаем тип следующего токена
-			if (jsonToken == JsonToken.BEGIN_OBJECT) { // если начало объекта
-				reader.beginObject();
-				System.out.println("{");
-			} else if (jsonToken == JsonToken.END_OBJECT) { // если конец объекта
-				reader.endObject();
-				System.out.println("}");
-			} else if (jsonToken == JsonToken.STRING) { // в случае если токен строковое знание - выводим на экран
-				System.out.println(reader.nextString() + " "); // напечатает Hi World!
-			} else if (jsonToken == JsonToken.BEGIN_ARRAY) { 
-				reader.beginArray();
-				System.out.println("[");
-			} else if (jsonToken == JsonToken.END_ARRAY) { 
-				reader.endArray();
-				System.out.println("]");
-			} else if (jsonToken == JsonToken.BOOLEAN) { 
-				System.out.println(reader.nextBoolean() + " ");
-			} else if (jsonToken == JsonToken.NUMBER) { 
-				System.out.println(reader.nextInt() + " ");
-			} else if (jsonToken == JsonToken.NAME) { 
-				System.out.println(reader.nextName() + ":");
-			} else {
-				reader.skipValue(); // пропускаем все прочие токены
-			}
-		}
-	}
+    private void parseRoot(JsonReader reader) throws IOException {
+        while (reader.hasNext()) {
+            JsonToken jsonToken = reader.peek();
+            if (jsonToken == JsonToken.BEGIN_OBJECT) {
+                reader.beginObject();
+            } else if (jsonToken == JsonToken.END_OBJECT) {
+                reader.endObject();
+            } else if (jsonToken == JsonToken.NAME) {
+                String name = reader.nextName();
+                if ("Called".equals(name)) {
+                    System.out.println(name + ":" + reader.nextString());
+                } else if ("Message".equals(name)) {
+                    System.out.println(name + ":" + reader.nextString());
+                } else if ("AvailableCalls".equals(name)) {
+                    parseCalls(reader);
+                }
+            } else {
+                reader.skipValue();
+            }
+        }
+    }
 
+    private void parseCalls(JsonReader reader) throws IOException {
+        while (reader.hasNext()) {
+            JsonToken jsonToken = reader.peek();
+            if (jsonToken == JsonToken.BEGIN_OBJECT) {
+                reader.beginObject();
+            } else if (jsonToken == JsonToken.END_OBJECT) {
+                reader.endObject();
+            } else if (jsonToken == JsonToken.NAME) {
+                String name = reader.nextName();
+                if ("RateLimit".equals(name)) {
+                    parseRateLimit(reader);
+                } else {
+                    parseService(reader);
+                }
+            } else {
+                reader.skipValue();
+            }
+        }
+    }
+
+    private void parseRateLimit(JsonReader reader) throws IOException {
+        while (reader.hasNext()) {
+            JsonToken jsonToken = reader.peek();
+            if (jsonToken == JsonToken.BEGIN_OBJECT) {
+                reader.beginObject();
+            } else if (jsonToken == JsonToken.END_OBJECT) {
+                reader.endObject();
+            } else if (jsonToken == JsonToken.NAME) {
+                System.out.println(reader.nextName() + "->");
+            } else {
+                reader.skipValue();
+            }
+        }
+    }
+
+    private void parseService(JsonReader reader) throws IOException {
+        while (reader.hasNext()) {
+            JsonToken jsonToken = reader.peek();
+            if (jsonToken == JsonToken.BEGIN_OBJECT) {
+                reader.beginObject();
+            } else if (jsonToken == JsonToken.END_OBJECT) {
+                reader.endObject();
+            } else if (jsonToken == JsonToken.NAME) {
+
+            } else {
+                reader.skipValue();
+            }
+        }
+    }
 }
